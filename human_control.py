c528ab678db4dd0f51571f44b2e7c337743dbef7
Szymon Sidor <szymon.sidor@gmail.com>
2015-11-13 14:03:45 -0500

    Updated deepq

---
+++ human_control.py
@@ -0,0 +1,31 @@
+from getch import getch
+from redis import StrictRedis
+
+
+
+class HumanController(object):
+    def __init__(self, mapping):
+        self.mapping = mapping
+        self.r = StrictRedis()
+        self.experience = []
+
+    def action(self, o):
+        return self.mapping[self.r.get("action")]
+
+    def store(self, observation, action, reward, newobservation):
+        pass
+
+    def training_step(self):
+        pass
+
+
+
+def control_me():
+    r = StrictRedis()
+    while True:
+        c = getch()
+        r.set("action", c)
+
+
+if __name__ == '__main__':
+    control_me()
