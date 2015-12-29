91c03873792eebb49027644627bbdfab7307a8ca
Szymon Sidor <szymon.sidor@gmail.com>
2015-12-29 23:31:20 +0100

    Updated the simulate.py code to be clearer and more accurate, i.e. the number of frames between actions is always the same - this makes code easier to read, and we can easily achieve maximum possible simulation speed (wait=False and bump up visualize_every). I also did not notice any performance decrease (in theory it could even boost performance slightly, because dt is guaranteed to be constant for every transition).

--- tf_rl/simulate.py
+++ tf_rl/simulate.py
@@ -1,17 +1,18 @@
+import math
 import time
 
 from IPython.display import clear_output, display, HTML
+from itertools import count
 from os.path import join, exists
 from os import makedirs
 
-from tf_rl.utils.event_queue import EventQueue
-
 def simulate(simulation,
-             controller = None,
+             controller= None,
              fps=60,
-             actions_per_simulation_second=60,
-             simulation_resultion=0.001,
-             speed=1.0,
+             visualize_every=1,
+             action_every=1,
+             simulation_resolution=None,
+             wait=False,
              disable_training=False,
              save_path=None):
     """Start the simulation. Performs three tasks
@@ -28,92 +29,83 @@ def simulate(simulation,
     controller: tr_lr.controller
         controller used
     fps: int
-        frames per seconds to display;
-        decrease for faster training.
-    actions_per_simulation_second: int
-        how many times perform_action is called per
-        one second of simulation time
-    speed: float
-        executed <speed> seconds of simulation time
-        per every second of real time
+        frames per seconds
+    visualize_every: int
+        visualize every `visualize_every`-th frame.
+    action_every: int
+        take action every `action_every`-th frame
+    simulation_resolution: float
+        simulate at most 'simulation_resolution' seconds at a time.
+        If None, the it is set to 1/FPS (default).
+    wait: boolean
+        whether to intentionally slow down the simulation
+        to appear real time.
     disable_training: bool
         if true training_step is never called.
     save_path: str
         save svg visualization (only tl_rl.utils.svg
         supported for the moment)
     """
-    eq = EventQueue()
 
-    time_between_frames  = 1.0 / fps
-    simulation_time_between_actions = 1.0 / actions_per_simulation_second
-
-    simulation_resultion /= speed
-
-    vis_s = {
-        'last_image': 0
-    }
-
+    # prepare path to save simulation images
     if save_path is not None:
         if not exists(save_path):
             makedirs(save_path)
+    last_image = 0
 
-    ###### VISUALIZATION
-    def visualize():
-        clear_output(wait=True)
-        svg_html = simulation.to_html()
-        display(svg_html)
-        if save_path is not None:
-            img_path = join(save_path, "%d.svg" % (vis_s['last_image'],))
-            with open(img_path, "w") as f:
-                svg_html.write_svg(f)
-            vis_s['last_image'] += 1
+    # calculate simulation times
+    chunks_per_frame = 1
+    chunk_length_s   = 1.0 / fps
 
-    eq.schedule_recurring(visualize, time_between_frames)
+    if simulation_resolution is not None:
+        frame_length_s = 1.0 / fps
+        chunks_per_frame = int(math.ceil(frame_length_s / simulation_resolution))
+        chunks_per_frame = max(chunks_per_frame, 1)
+        chunk_length_s = frame_length_s / chunks_per_frame
 
+    # state transition bookkeeping
+    last_observation = None
+    last_action      = None
 
-    ###### CONTROL
-    ctrl_s = {
-        'last_observation': None,
-        'last_action':      None,
-    }
+    simulation_started_time = time.time()
 
-    def control():
-        # sense
-        new_observation = simulation.observe()
-        reward          = simulation.collect_reward()
-        # store last transition
-        if ctrl_s['last_observation'] is not None:
-            controller.store(ctrl_s['last_observation'], ctrl_s['last_action'], reward, new_observation)
+    for frame_no in count():
+        for _ in range(chunks_per_frame):
+            simulation.step(chunk_length_s)
 
-        # act
-        new_action = controller.action(new_observation)
-        simulation.perform_action(new_action)
+        if frame_no % action_every == 0:
+            new_observation = simulation.observe()
+            reward          = simulation.collect_reward()
+            # store last transition
+            if last_observation is not None:
+                controller.store(last_observation, last_action, reward, new_observation)
 
-        #train
-        if not disable_training:
-            controller.training_step()
+            # act
+            new_action = controller.action(new_observation)
+            simulation.perform_action(new_action)
 
-        # update current state as last state.
-        ctrl_s['last_action'] = new_action
-        ctrl_s['last_observation'] = new_observation
+            #train
+            if not disable_training:
+                controller.training_step()
 
+            # update current state as last state.
+            last_action = new_action
+            last_observation = new_observation
 
+        # adding 1 to make it less likely to happen at the same time as
+        # action taking.
+        if (frame_no + 1) % visualize_every == 0:
+            fps_estimate = frame_no / (time.time() - simulation_started_time)
+            clear_output(wait=True)
+            svg_html = simulation.to_html(["fps = %.1f" % (fps_estimate,)])
+            display(svg_html)
+            if save_path is not None:
+                img_path = join(save_path, "%d.svg" % (last_image,))
+                with open(img_path, "w") as f:
+                    svg_html.write_svg(f)
+                last_image += 1
 
-    ##### SIMULATION
-    sim_s = {
-        'simulated_up_to':             time.time(),
-        'simulation_time_since_last_action': 0,
-    }
-    def advance_simulation():
-        while sim_s['simulated_up_to'] < time.time():
-            simulation.step(simulation_resultion)
-            sim_s['simulated_up_to'] += simulation_resultion / speed
-            sim_s['simulation_time_since_last_action'] += simulation_resultion
-            if sim_s['simulation_time_since_last_action'] > simulation_time_between_actions:
-                if controller is not None:
-                    control()
-                    sim_s['simulation_time_since_last_action'] = 0
-
-    eq.schedule_recurring(advance_simulation, time_between_frames)
-
-    eq.run()
+        time_should_have_passed = frame_no / fps
+        time_passed = (time.time() - simulation_started_time)
+        if wait and (time_should_have_passed > time_passed):
+            time.sleep(time_should_have_passed - time_passed)
